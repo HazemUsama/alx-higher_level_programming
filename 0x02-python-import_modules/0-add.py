6a55ae8b6206a23cedfdc7089ce3c5cfd2926053
HazemUsama <hazemosama681@gmail.com>
2023-08-12 02:01:56 +0300

    fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import add_0
-a = 1
-b = 2
-print(a + " + " + b + " = " + add_0.add(a, b))
+if __name__ == "__main__":
+    from add0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
