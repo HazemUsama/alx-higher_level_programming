da209a596b1c703489a7e13ef27c26a752ee0576
HazemUsama <hazemosama681@gmail.com>
2023-08-31 00:15:40 +0300

    first task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(args, x):
+    for i in range(x):
+        try:
+            print(args[i], end=" ")
+        except IndexError:
+            break
+    print("")
