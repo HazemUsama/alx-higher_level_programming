2fcc4577ee0a3d4a45da4a76e83db4a328e6c908
HazemUsama <hazemosama681@gmail.com>
2023-08-31 05:16:14 +0300

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""" it's a Square """
+
+
+class Square:
+    """"
+    Still a  Square
+    Attributes:
+        __size (int): size of the Square
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of square
+        Returns: None
+        """
+        self.__size = size
