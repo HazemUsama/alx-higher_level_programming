83491ddc1ad086ff35d202a3e204d2c8234466f4
HazemUsama <hazemosama681@gmail.com>
2023-09-05 05:21:39 +0300

    done

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Start of the file """
+
+
+class Rectangle:
+    """docstring for Rectangle."""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """The width property."""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError('width must be >= 0')
+            else:
+                self.__width = value
+        else:
+            raise TypeError('width must be an integer')
+
+    @@property
+    def height(self):
+        """The height property."""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError('height must be >= 0')
+            else:
+                self.__height = value
+        else:
+            raise TypeError('height must be an integer')
