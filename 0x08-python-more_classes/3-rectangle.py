638bdca0e782d59ae8bc0e31c1a16a9da94ce8d7
HazemUsama <hazemosama681@gmail.com>
2023-09-05 05:37:36 +0300

    done

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Start of the file """
+
+
+class Rectangle:
+    """docstring for Rectangle."""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """The width property."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError('width must be >= 0')
+            else:
+                self.__width = value
+        else:
+            raise TypeError('width must be an integer')
+
+    @property
+    def height(self):
+        """The height property."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError('height must be >= 0')
+            else:
+                self.__height = value
+        else:
+            raise TypeError('height must be an integer')
+
+    def area(self):
+        """ return the area """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * self.__width + 2 * self.__height
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect_str = "#" * self.__width + "\n"
+        rect_str *= self.__height - 1
+        rect_str += "#" * self.__width
+        return rect_str
+
