9c0c2e12f8032711a9d480637db2853d3876ea4b
HazemUsama <hazemosama681@gmail.com>
2023-09-06 03:43:49 +0300

    style error

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,4 +4,5 @@ class LockedClass:
         if name == 'first_name':
             super().__setattr__(name, value)
         else:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+            raise AttributeError("'LockedClass' object has no attribute '{}'"
+                                 .format(name))
