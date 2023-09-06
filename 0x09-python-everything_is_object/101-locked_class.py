54653bdc606e7940f5fdde01ac1f17b827e56ecf
HazemUsama <hazemosama681@gmail.com>
2023-09-06 03:50:45 +0300

    idk

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class LockedClass:
     def __setattr__(self, name, value):
-        if name == 'first_name':
-            super().__setattr__(name, value)
+        if hasattr(self, name) or name == 'first_name':
+            object.__setattr__(self, name, value)
         else:
             raise AttributeError("'LockedClass' object has no attribute '{}'"
                                  .format(name))
