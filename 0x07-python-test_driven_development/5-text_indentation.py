7b722742ecaa3ca178ba75e8457a4c0ee1f7df72
HazemUsama <hazemosama681@gmail.com>
2023-09-07 00:56:50 +0300

    done

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""5-text_indentation"""
+
+
+def text_indentation(text):
+    """Print with indentation"""
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    for ch in text:
+        print(ch, end='')
+        if ch in '.?:':
+            print('\n')
