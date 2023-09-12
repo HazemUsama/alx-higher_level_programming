f5fdb78bbf73877ae385d2f1906a27f539121962
HazemUsama <hazemosama681@gmail.com>
2023-09-12 15:02:26 +0300

    done

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""2-is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if the object is exactly an
+     instance of the specified class
+    """
+    return issubclass(obj, a_class)
