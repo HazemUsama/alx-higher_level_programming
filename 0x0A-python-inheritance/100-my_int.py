953d8b8d2c7a2deeeb02a7bf4b4b7b98802c5da4
HazemUsama <hazemosama681@gmail.com>
2023-09-12 18:17:09 +0300

    done

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,13 @@ class MyInt(int):
     """invert == and !="""
 
     def __eq__(self, value):
-        return super().__ne__(self, value)
+        return super().__ne__(value)
 
     def __ne__(self, value):
-        return super().__eq__(self, value)
+        return super().__eq__(value)
+
+
+my_i = MyInt(3)
+print(my_i)
+print(my_i == 3)
+print(my_i != 3)
