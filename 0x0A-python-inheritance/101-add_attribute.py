e515f224c9a2e7c4d1062ccc4126661ed455c5ae
HazemUsama <hazemosama681@gmail.com>
2023-09-12 18:36:04 +0300

    done

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,8 @@
 
 
 def add_attribute(self, attr_name, value):
+    """check if the class can have attributes"""
+
     if hasattr(self, '__dict__'):
         setattr(self, attr_name, value)
     else:
