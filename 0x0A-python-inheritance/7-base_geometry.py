b70e8d6554c175ddfd3be6770b6ea340474bf3ba
HazemUsama <hazemosama681@gmail.com>
2023-09-12 16:19:04 +0300

    done

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""7-base_geometry"""
+
+
+class BaseGeometry:
+    """Improved Class"""
+
+    def area(slef):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
