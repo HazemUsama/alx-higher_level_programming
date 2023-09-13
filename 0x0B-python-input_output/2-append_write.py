ca954371de94852295f8bed1f1caee8cd127fc58
HazemUsama <hazemosama681@gmail.com>
2023-09-13 03:27:25 +0300

    done

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""only on function append_write"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding="UTF8") as f:
+        f.write(text)
+        
+
