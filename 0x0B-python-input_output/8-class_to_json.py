d9721ea06e7c2648046cd25fe996031bfe76196b
HazemUsama <hazemosama681@gmail.com>
2023-09-13 04:20:07 +0300

    ??

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,8 +7,4 @@ def class_to_json(obj):
      data structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
     """
-    json_dict = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            json_dict[key] = value
-    return json_dict
+    return obj.__dict__
