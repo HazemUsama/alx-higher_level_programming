2a47c7b2dcfaf46c190a99eb9f57d49a029ce803
HazemUsama <hazemosama681@gmail.com>
2023-09-13 03:45:17 +0300

    write not read

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@ def save_to_json_file(my_obj, filename):
     writes an Object to a text file,
     using a JSON representation
     """
-    with open(filename) as f:
+    with open(filename, 'w') as f:
         json.dump(my_obj, f)
