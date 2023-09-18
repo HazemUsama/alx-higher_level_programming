7cc61823509483d0fd3e05444237911148b95c4f
HazemUsama <hazemosama681@gmail.com>
2023-09-19 00:59:21 +0300

    done>

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,17 +89,22 @@ class Rectangle(Base):
         return f'[Rectangle] ({self.id}) {self.x}/{self.y} - \
 {self.width}/{self.height}'
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
         Update attributes
         """
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.__width = args[1]
-        if len(args) >= 3:
-            self.__height = args[2]
-        if len(args) >= 4:
-            self.__x = args[3]
-        if len(args) >= 5:
-            self.__y = args[4]
+        if args is not None:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        else:
+            for key, value in kwargs:
+                if hasattr(self, key):
+                    setattr(self, key, value)
