378c0485653c8fbe9621e1e37c38d0099b431264
HazemUsama <hazemosama681@gmail.com>
2023-09-19 00:56:16 +0300

    style fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,7 +94,7 @@ class Rectangle(Base):
         Update attributes
         """
         if len(args) >= 1:
-            self.id = args[0] 
+            self.id = args[0]
         if len(args) >= 2:
             self.__width = args[1]
         if len(args) >= 3:
@@ -103,4 +103,3 @@ class Rectangle(Base):
             self.__x = args[3]
         if len(args) >= 5:
             self.__y = args[4]
-
