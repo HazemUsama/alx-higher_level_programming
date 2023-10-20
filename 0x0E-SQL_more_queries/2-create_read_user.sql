a9e93b7156f9ac319d13d945d358d71edeef7b24
HazemUsama <hazemosama681@gmail.com>
2023-10-20 04:28:55 +0300

    ugh

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,6 @@ CREATE DATABASE
 CREATE USER 
 	IF NOT EXISTS `user_0d_2`@`localhost`
 	IDENTIFIED BY `user_0d_2_pwd`;
-GRANT SELECT ON hbtn_0d_2.* 
+GRANT SELECT
+	ON `hbtn_0d_2`.* 
 	TO `user_0d_2`@`localhost`; 
