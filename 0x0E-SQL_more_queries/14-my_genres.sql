8064933f1b2682014bf584b58e9bdeec0525e19e
HazemUsama <hazemosama681@gmail.com>
2023-10-23 03:31:52 +0300

    again

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,8 @@
 -- lists all genres of the show Dexter
 SELECT tv_genres.name
 FROM tv_genres JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genres_id
-WHERE tv_show_genres.name = "Dexter"
+ON tv_genres.id = (
+	SELECT id
+	FROM tv_shows
+	WHERE tv_shows.name = "Dexter"
+)
