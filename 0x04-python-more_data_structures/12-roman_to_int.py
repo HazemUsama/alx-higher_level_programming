1d7bda93976ac53d6c5a7249c13404a73c7a23a0
HazemUsama <hazemosama681@gmail.com>
2023-11-03 10:22:57 +0200

    done

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    ans = 0
+    for idx, char in enumerate(roman_string):
+        try:
+            if roman[roman_string[idx]] < roman[roman_string[idx + 1]]:
+                ans -= roman[char]
+                continue
+        except:
+            pass
+        ans += roman[char]
+    return ans
