5e314b24743f74fcd132d37cdae6966199a1610c
HazemUsama <hazemosama681@gmail.com>
2023-11-03 09:43:07 +0200

    style fix

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    new_set =  {item for item in set_1 if item not in set_2}
+    new_set = {item for item in set_1 if item not in set_2}
     new_set = new_set.union({item for item in set_2 if item not in set_1})
     return new_set
