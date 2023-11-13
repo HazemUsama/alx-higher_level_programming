3a1185fced6ed57821e3240729785d54a1bab13f
HazemUsama <hazemosama681@gmail.com>
2023-11-13 17:46:29 +0200

    done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
+let args = process.argv.length - 2
 
-if (argv.length === 2) console.log('No argument');
-else if (argv.length === 3) console.log('Argument found');
+if (!args.length) console.log('No argument');
+else if (args.length === 1) console.log('Argument found');
 else console.log('Arguments found');
