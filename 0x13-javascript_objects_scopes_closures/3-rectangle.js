e5d78dde3ab77d83b5b42cf017483399b5229608
HazemUsama <hazemosama681@gmail.com>
2023-11-14 20:35:12 +0200

    fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,10 +8,10 @@ class Rectangle {
     }
   }
 
-  print() {
-    for (let i = 0; i < num; i++) {
+  print () {
+    for (let i = 0; i < this.height; i++) {
       let row = '';
-      for (let j = 0; j < num; j++) {
+      for (let j = 0; j < this.width; j++) {
         row += 'X';
       }
       console.log(row);
