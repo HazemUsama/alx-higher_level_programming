56cb849eac5ec74f049e1621479510da1cb89880
HazemUsama <hazemosama681@gmail.com>
2023-11-19 23:18:52 +0200

    done

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,7 +2,8 @@
 """
 The class definition of a City
 """
-from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, VARCHAR
+from sqlalchemy import create_engine, ForeignKey,\
+        Column, String, Integer, VARCHAR
 from model_state import Base, State
 from sqlalchemy.orm import declarative_base
 
@@ -16,4 +17,5 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column('id', Integer, primary_key=True)
     name = Column('name', VARCHAR(128), nullable=False)
-    state_id = Column('state_id', Integer, ForeignKey('State.id'), nullable=False)
+    state_id = Column('state_id', Integer,
+                      ForeignKey('State.id'), nullable=False)
