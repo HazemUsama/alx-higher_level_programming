d1fad31c1bbc234f0a664a25a1ee52d553100ed2
HazemUsama <hazemosama681@gmail.com>
2023-11-19 19:09:32 +0200

    i really don't know

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,19 +2,17 @@
 """
 Script that lists all states from the database hbtn_0e_0_usa.
 """
+import MySQLdb
+import sys
 
 
 if __name__ == '__main__':
-    import MySQLdb
-    import sys
 
-    UserName = sys.argv[1]
-    UserPass = sys.argv[2]
-    DataBaseName = sys.argv[3]
+username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=UserName,
-                           passwd=UserPass, db=DataBaseName)
-    cur = conn.cursor()
+conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                                    passwd=password, db=database)
+cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,9 +3,11 @@
 Script that lists all states from the database hbtn_0e_0_usa
 with a name starting with N.
 """
+import MySQLdb
+import sys
+
+
 if __name__ == '__main__':
-    import MySQLdb
-    import sys
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
