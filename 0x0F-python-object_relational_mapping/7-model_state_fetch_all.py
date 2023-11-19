77b741baae48b30ddfa50ac5052617cb5179ce9d
HazemUsama <hazemosama681@gmail.com>
2023-11-19 22:07:37 +0200

    fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
                            (username, password, database), pool_pre_ping=True)
-    #Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
