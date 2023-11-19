3b4f910a6c2b624e619a77f5aa06346f72751a2e
HazemUsama <hazemosama681@gmail.com>
2023-11-19 20:13:21 +0200

    try this

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+where name matches the argument (safe this time)
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    if "\'" in name:
+        exit()
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(name))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
