613c08bd2c9e8c372a1cdbf45c27c815b7a1f339
HazemUsama <hazemosama681@gmail.com>
2023-11-19 19:00:56 +0200

    fixed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+with a name starting with N.
+"""
 if __name__ == '__main__':
     import MySQLdb
     import sys
