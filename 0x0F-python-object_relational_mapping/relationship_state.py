70e33c364ea9859dd3cf752b44810114b8d67f92
HazemUsama <hazemosama681@gmail.com>
2023-11-20 02:16:40 +0200

    done?

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+The class definition of a State
+"""
+from sqlalchemy import create_engine, Column, String, Integer, VARCHAR
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Create the Class State
+    """
+    __tablename__ = 'states'
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', VARCHAR(128), nullable=False)
+    cities = relationship('City', back_populates='states')
