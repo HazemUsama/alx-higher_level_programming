e007d31d40998908db8fd72669460c5e3a7f451a
HazemUsama <hazemosama681@gmail.com>
2023-12-06 20:53:54 +0200

    third one done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, values)
+    with urllib.request.urlopen(req) as res:
+        print(res.read())
