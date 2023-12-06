a0298d3a6926a875754716068b074cbacabe0bd5
HazemUsama <hazemosama681@gmail.com>
2023-12-06 22:11:04 +0200

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,10 +10,12 @@ if __name__ == '__main__':
     except:
         par = ""
 
-    res = requests.post('http://0.0.0.0:5000/search_user', params=par).json()
-    if not isinstance(res, dict):
+    r = requests.post('http://0.0.0.0:5000/search_user', params=par)
+    try:
+        res = r.json()
+        if res == {}:
+            print('No result')
+        else:
+            print('[{}]: {}'.format(res.get('id'), res.get('name'))
+    except:
         print('Not a valid JSON')
-    elif res == {}:
-        print('No result')
-    else:
-        print('[{}]: {}'.format(res['id'], res['name']))
