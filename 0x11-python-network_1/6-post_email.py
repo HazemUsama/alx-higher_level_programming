9dd41eb31ba55cec638ef359f98f9def32169d3e
HazemUsama <hazemosama681@gmail.com>
2023-12-06 21:26:29 +0200

    done?

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL"""
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    res = requests.post(url, data={'email', email})
+    print(res)
