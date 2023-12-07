c38dbe32aa1e715e160759b97e17513c9ca24248
HazemUsama <hazemosama681@gmail.com>
2023-12-07 18:35:07 +0200

    done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""display your github id"""
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url)
+    res = r.json()
+    for i in range(5):
+        name = res[i].get('commit').get('author').get('name')
+        print('{}: {}'.format(res[i].get('sha'), name))
