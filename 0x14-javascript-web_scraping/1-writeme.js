5b67c6f45ebaa4f3b9da0373818be974fe9bf932
HazemUsama <hazemosama681@gmail.com>
2024-02-01 04:43:45 +0200

    Fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,6 @@
 // import file system module
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], 'utf8', (err, data) => {
-  console.log(err || data);
-});
-
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
   if (error) console.log(error);
 });
