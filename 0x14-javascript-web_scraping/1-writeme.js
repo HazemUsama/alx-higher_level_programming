ac6d38db89690f8ecd852e26eeeecfd0f24c0eee
HazemUsama <hazemosama681@gmail.com>
2024-02-01 04:42:55 +0200

    write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// import file system module
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], 'utf8', (err, data) => {
+  console.log(err || data);
+});
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
+  if (error) console.log(error);
+});
